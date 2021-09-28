4b2479e22cbcb328f92fa74e39aaa32f989aab11
ilai2 <isabel_lai@brown.edu>
2021-09-27 22:46:40 -0400

    class 9 notes and bignum release

--- src/Info/Notes/Classes/Classes.js
+++ src/Info/Notes/Classes/Classes.js
@@ -221,7 +221,7 @@ const classes = [
 	{
 		number: 9, title: "Recursive Evaluation", outDate: "Sept. 27", active: true,
 		links: [
-			// { icon: <FaFileAlt />, link: "", label: "Notes" },
+			{ icon: <FaFileAlt />, link: "https://drive.google.com/file/d/1pdthg4NUa3UxQF9pYBfWgJ23TydxgYIt/view?usp=sharing", label: "Notes" },
 			{ icon: <FaYoutube />, link: "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=252bb686-f7b7-4200-bf07-ad9d000ea4d5", label: "Recording" },
 			{ icon: <FaFilePowerpoint />, link: "https://docs.google.com/presentation/d/1E28nTGxjDt_k8LFWfE4hjiZoF0JMEokP/edit?usp=sharing&ouid=106478871100678252393&rtpof=true&sd=true", label: "Slides" },
 		],
