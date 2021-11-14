a76505bf3e77cf864f78aee0928b1d5f16c5e6c0
df3balex <alexandra_christine_ryan@brown.edu>
2021-11-14 13:37:04 -0500

    fixing lab 10 lol

--- src/Info/Assignments/Labs/Labs.js
+++ src/Info/Assignments/Labs/Labs.js
@@ -70,7 +70,7 @@ const labs = [
 		]
 	},
 	{
-		number: 10, title: "Game", dueDate: "Nov. 14-16", active: false,
+		number: 10, title: "Game", dueDate: "Nov. 14-16", active: true,
 		links: [
 			{ icon: <FaFileAlt />, link: "https://drive.google.com/file/d/1eroUL0UBdgEa6oIy1qLs9S1Wnghe7BOZ/view?usp=sharing", label: "Handout" },
 			{ icon: <FaCode />, link: "https://drive.google.com/drive/folders/1-SVnY6ER-eO9WfwX2KJ-BW26voi0W49H?usp=sharing", label: "Source Code" },
