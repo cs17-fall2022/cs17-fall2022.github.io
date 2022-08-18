63d845169bb4ce4a573f97792d327ad19379348c
ilai2 <isabel_lai@brown.edu>
2022-08-18 13:51:13 -0400

    project wipe

--- src/Info/Assignments/Projects/Projects.js
+++ src/Info/Assignments/Projects/Projects.js
@@ -2,7 +2,7 @@ import { FaFileAlt, FaCode, FaFilePowerpoint, FaYoutube, } from 'react-icons/fa'
 
 const projects = [
 	{
-		number: 1, title: "Bignum", dueDate: "Oct. 14, 11 p.m. EDT", active: true,
+		number: 1, title: "Bignum", dueDate: "Oct. 14, 11 p.m. EDT", active: false,
 		notes: "",
 		links: [
 			{ icon: <FaFileAlt />, link: "", label: "Handout" },
@@ -12,7 +12,7 @@ const projects = [
 		]
 	},
 	{
-		number: 2, title: "Rackette", dueDate: "Nov. 12, 11 p.m. EST", active: true,
+		number: 2, title: "Rackette", dueDate: "Nov. 12, 11 p.m. EST", active: false,
 		notes: "",
 		links: [
 			{ icon: <FaFileAlt />, link: "", label: "Handout" },
@@ -22,7 +22,7 @@ const projects = [
 		]
 	},
 	{
-		number: 3, title: "Game", dueDate: "Dec. 6, 11 p.m. EST", active: true,
+		number: 3, title: "Game", dueDate: "Dec. 6, 11 p.m. EST", active: false,
 		notes: "",
 		links: [
 			{ icon: <FaFileAlt />, link: "", label: "Handout" },
