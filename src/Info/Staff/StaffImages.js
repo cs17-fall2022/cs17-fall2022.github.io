9a25d1705f4a309c50ba526e1982e9e27d361f94
thhtzaw <thet_htay_zaw@brown.edu>
2022-08-29 15:36:36 -0400

    Remove old staff images

--- src/Info/Staff/StaffImages.js
+++ src/Info/Staff/StaffImages.js
@@ -1,30 +1,7 @@
-import Alyssa from './Images/Alyssa.JPG';
-import April from './Images/April.JPG';
-import Austin from './Images/Austin.JPG';
-import Bryanna from './Images/Bryanna.JPG';
-import Cali from './Images/Cali.JPG';
-import Ian from './Images/Ian.JPG';
-import Clark from './Images/Clark.JPG';
-import Colby from './Images/Colby.JPG';
-import Ethan from './Images/Ethan.JPG';
-import Harshini from './Images/Harshini.JPG';
-import Maceo from './Images/Maceo.JPG';
-import MinhQuan from './Images/MinhQuan.JPG'
-import Neil from './Images/Neil.JPG';
-import Qinan from './Images/Qinan.JPG';
-import Vandana from './Images/Vandana.JPG';
-import Wonyoung from './Images/Wonyoung.JPG';
-import Zeynep from './Images/Zeynep.JPG';
-import Isabel from './Images/Isabel.JPG';
-import Stephen from './Images/Stephen.JPG';
-import Alex from './Images/Alex.JPG';
-import Thet from './Images/Thet.JPG';
-import Spike from './Images/Spike.jpg';
+import Default from './Images/Default.png';
 
 const staffImages = {
-	Alyssa, April, Austin, Bryanna, Cali, Ian, Clark, Colby, Ethan, Harshini,
-	Maceo, MinhQuan, Neil, Qinan, Vandana, Wonyoung, Zeynep, Isabel, Stephen,
-	Alex, Thet, Spike
+	Default
 }
 
 export default staffImages;
