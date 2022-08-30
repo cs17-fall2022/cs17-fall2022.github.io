a635ed676c48918ab430b28129fc83e9753dbf38
thhtzaw <thet_htay_zaw@brown.edu>
2022-08-30 10:33:00 -0400

    Add Caleb, Melinda, and Stephen

--- src/Info/Staff/Staff.js
+++ src/Info/Staff/Staff.js
@@ -13,9 +13,29 @@ const staff = [
 	},
 	{
 		name: "Kai", login: "kzuang", image: staffImages.Default,
-		hometown: "Frisco, Texas", birthday: "April 6", pronouns: "he/him/his",
+		hometown: "Frisco, TX", birthday: "April 6", pronouns: "he/him/his",
 		bio: "Hi guys im a sophomore planning on concentrating in cs. When i'm not busy failing interviews i like to spend my free time catching sunsets, stargazing, or lying in the grass :)"
 	},
+	{
+		name: "Stephen", login: "schen211", image: staffImages.Default, 
+		hometown: "Plymouth, MN", birthday: "September 29", pronouns: "he/him/his",
+		bio: "Hi! I'm a senior studying CS from Plymouth, Minnesota (right next to the Twin Cities). In my free time, you can find me either listening to the same three Taylor Swift songs on loop or losing in League of Legends."
+	},
+	{
+		name: "Caleb", login: "cmoran5", image: staffImages.Default,
+		hometown: "Arlington, VA", birthday: "December 17", pronouns: "he/him/his",
+		bio: "Howdy! I’m a sophomore likely concentrating in APMA–CS. I’m passionate about breakfast at the VDub and play for the men’s Ultimate Frisbee A Team (currently #2 in the country). Let’s chat about favorite movies, herbal teas, and why the Hay is the best library"
+	},
+	{
+		name: "Melinda", login: "mzhan157", image: staffImages.Default,
+		hometown: "Andover, MA", birthday: "August 12", pronouns: "she/her/hers",
+		bio: "Hi everyone!! I'm a sophomore planning to concentrate in computational biology. I love cooking and trying new foods, drinking chai/matcha in the engineering cafe, spending time with friends, listening to music (always open to recs!), and going on night walks. I'm so excited to meet and work with you all :))"
+	}
+	// {
+	// 	name: "", login: "", image: staffImages.Default,
+	// 	hometown: "", birthday: "", pronouns: "",
+	// 	bio: ""
+	// }
 ]
 
 export default staff;
