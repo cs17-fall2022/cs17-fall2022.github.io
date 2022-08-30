8fb1b74684b0fc9a3e79d8ece868c66609c0c9eb
thhtzaw <thet_htay_zaw@brown.edu>
2022-08-30 10:04:31 -0400

    Changing stuff

--- src/Info/Staff/Staff.js
+++ src/Info/Staff/Staff.js
@@ -9,7 +9,7 @@ const staff = [
 	{
 		name: "Vandana (HTA)", login: "vramesh1", image: staffImages.Default,
 		hometown: "Mumbai, India", birthday: "January 24", pronouns: "she/her/hers",
-		bio: "Hi!! I’m a senior studying CS and Economics. I love chocolate, fruits, and the colour blue! When I’m not at the CIT, I’m usually dancing, studying languages (the older, the better), reading, or doing art!"
+		bio: "Hi!! I’m a senior studying CS and Economics. I’m also doing a concurrent Master’s in CS and I’d be happy to answer questions about it. I love chocolate, fruits, and the colour blue! When I’m not at the CIT, I’m usually dancing, studying languages (the older, the better), reading, or doing art!"
 	},
 	{
 		name: "Kai", login: "kzuang", image: staffImages.Default,
