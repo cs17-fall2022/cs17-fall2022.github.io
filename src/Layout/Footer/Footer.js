efa8ac7770df3c5ae3384f4763481f8a7c431923
thhtzaw <thet_htay_zaw@brown.edu>
2022-09-04 16:21:54 -0400

    Update copyright

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
 
 		return (
 			<div className={"footer-wrapper" + resizeStyle}>
-				<div>Copyright © 2021 CS17</div>
+				<div>Copyright © 2022 CS17</div>
 				<div>Submit feedback at our anonymous form <a href="https://forms.gle/Fk9jZpGrgRgeZXDg6" target="_blank" rel="noreferrer noopener">here!</a></div>
 			</div>
 		);
